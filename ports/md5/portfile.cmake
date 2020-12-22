61955c84b31f5b7c844758b737b5f6f7435a4fe0
yao.zhao <yao.zhao@dms365.com>
2020-12-22 10:49:46 +0800

    update md5 port with export targets

--- ports/md5/portfile.cmake
+++ ports/md5/portfile.cmake
@@ -8,7 +8,11 @@ vcpkg_from_github(
     SHA512 135b4e24697035528aab42205e0d7934aff96fc57b942cd5aeb039bfecc9e755483a6a3b3fab5209bcc3966aecf9c5cd65ae410beb11a9796fd5e4dfc3baa406
 )
 
-file(COPY ${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt DESTINATION ${SOURCE_PATH})
+file(COPY 
+    ${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt 
+    ${CMAKE_CURRENT_LIST_DIR}/md5-config.in.cmake
+    DESTINATION ${SOURCE_PATH}
+)
 
 vcpkg_configure_cmake(
     SOURCE_PATH ${SOURCE_PATH}
@@ -16,6 +20,8 @@ vcpkg_configure_cmake(
 )
 
 vcpkg_install_cmake()
+
+vcpkg_fixup_cmake_targets(CONFIG_PATH share/unofficial-${PORT} TARGET_PATH share/unofficial-${PORT})
 
 file(REMOVE_RECURSE 
     "${CURRENT_PACKAGES_DIR}/debug/include"
